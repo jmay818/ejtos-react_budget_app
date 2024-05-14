44b03a32007e277a1267dff279443d6a6accad03
JMay <jmay818@gmail.com>
2024-05-14 02:02:18 -0400

    Updated app

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -15,7 +15,7 @@ const AllocationForm = (props) => {
                 setCost("");
                 return;
             }
-
+            
         const expense = {
             name: name,
             cost: parseInt(cost),
@@ -46,7 +46,7 @@ const AllocationForm = (props) => {
                         <option value="Marketing" name="marketing"> Marketing</option>
                 <option value="Sales" name="sales">Sales</option>
                 <option value="Finance" name="finance">Finance</option>
-                <option value="HR" name="hr">HR</option>
+                <option value="Human Resource" name="hr">HR</option>
                 <option value="IT" name="it">IT</option>
                 <option value="Admin" name="admin">Admin</option>
                   </select>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,32 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch, expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+        if (totalExpenses > event.target.value) {
+            alert("You cannot reduce the budget value lower than the spending");
+            setNewBudget(totalExpenses)
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: totalExpenses
+            });
+            document.getElementById('budget').value = totalExpenses;
+        } else {
+            setNewBudget(event.target.value);
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: event.target.value
+            });
+        }
     }
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input id="budget" type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
